413978f6d8f8ea3742b7a665ce591f5555c6eb11
Joshua Yang <joshuayang@mac.com>
2022-07-27 18:38:39 +0800

    Trying out new ways to implement the grid inputs Instead of individual grid listening to gestures, the grid listens to the gestures

--- Pathfinding_Visualization/ContentView.swift
+++ Pathfinding_Visualization/ContentView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var model = Model(maxRow: 30, maxColumn: 40)
-    @State var mouseDown = false
-    var isDragging = false
+    @GestureState private var location: CGPoint = .zero
+    
+    func rectReader(node: Node) -> some View {
+        return GeometryReader { (geometry) -> AnyView in
+            if geometry.frame(in: .global).contains(self.location) {
+                DispatchQueue.main.async {
+                    node.toggleWall()
+                }
+            }
+            
+            return AnyView(Rectangle().fill(Color.clear))
+            
+        }
+    }
     
     var body: some View {
         NavigationView {
             VStack(spacing: 1) {
-                ForEach(model.grid, id: \.self) { row in
+                ForEach(0 ..< model.grid.count, id: \.self) { rowIndex in
                     HStack(spacing: 1) {
-                        ForEach(row) { node in
-                            NodeView(nodeInfo: node, mouseDown: $mouseDown)
+                        ForEach(0 ..< model.grid[0].count) { columnIndex in
+                            NodeView(nodeInfo: model.grid[rowIndex][columnIndex])
                         }
                     }
                 }
--- Pathfinding_Visualization/NodeView.swift
+++ Pathfinding_Visualization/NodeView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct NodeView: View {
     @State var nodeInfo: Node
-    @Binding var mouseDown: Bool
     
     
     private func nodeColor(state: NodeState) -> Color {
@@ -34,34 +33,12 @@ struct NodeView: View {
         Rectangle()
             .fill(nodeColor(state: nodeInfo.getState()))
             .frame(width: 25, height: 25)
-            .onHover { hover in
-                if mouseDown && hover {
-                    print("mouse hover")
-                    nodeInfo.toggleWall()
-                }
-                if hover {
-                    print("node: \(nodeInfo.id) hovered!")
-                }
-            }
-            .pressAction {
-                if mouseDown == false {
-                    mouseDown = true
-                    print("mouse down")
-                    nodeInfo.toggleWall()
-                }
-            } onRelease: {
-                mouseDown = false
-                print("mouse up")
-            }
-        
-                
 
-    
     }
 }
 
 struct NodeView_Previews: PreviewProvider {
     static var previews: some View {
-        NodeView(nodeInfo: Node(as: .start(false)), mouseDown: .constant(false))
+        NodeView(nodeInfo: Node(as: .start(false)))
     }
 }
